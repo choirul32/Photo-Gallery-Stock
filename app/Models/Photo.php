38bfc9e4d2a5c6d5b99a766873acfa4098e78b4e
Muhammad Choirul Anwar <choirul32anwar@gmail.com>
2020-12-25 23:54:41 +0700

    *fix any bug

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -9,11 +9,17 @@ class Photo extends Model
 {
     use HasFactory;
 
+    protected $table = 'photos';
+
     protected $fillable = [
         'src', 'name', 'user_id'
     ];
 
     public function user(){
-        return $this->belongsTo('App\Models\User');
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
+
+    public function tag(){
+        return $this->belongsToMany('App\Models\Tag', 'photo_tag', 'photo_id', 'tag_id');
     }
 }
